f55e6f26855c66dc14ed41ccbf6493ee50e2521d
CocaArav2026 <githubprouser@gmail.com>
2026-04-25 02:14:45 +0600

    Update pp.js

--- Script/commands/pp.js
+++ Script/commands/pp.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "pp",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "Coca Arav",
  description: "Lấy ID người dùng.",
  commandCategory: "Công cụ",
  cooldowns: 0
@@ -43,7 +43,9 @@ var data = await api.getUserInfoV2(res_ID);
 		if (args.join().indexOf('@') !== -1) 
  var uid = Object.keys(event.mentions) 
  var callback = () => 
-api.sendMessage({body:`=== profile===`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+api.sendMessage({body:`『✦』────────『✦』
+  𝐁𝐚𝐛𝐲, 𝐇𝐞𝐫𝐞'𝐬 𝐲𝐨𝐮𝐫 𝐩𝐫𝐨𝐟𝐢𝐥𝐞 😘
+『✦』────────『✦』`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
  () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
  return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
  () => callback()); 
