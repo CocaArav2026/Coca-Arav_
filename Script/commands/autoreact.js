64ccaf38f8cc63d941df2529cafea91578587061
CocaArav2026 <githubprouser@gmail.com>
2026-04-24 20:08:50 +0600

    Update autoreact.js

--- Script/commands/autoreact.js
+++ Script/commands/autoreact.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "autoreact",
  version: "1.1.1",
  hasPermission: 0,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "𝐀𝐫𝐚𝐯 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭",
  description: "Bot React",
  commandCategory: "No Prefix",
  cooldowns: 0,
